d247bed2762648997066071f9cd46e0235386c52
Daniel Shir <danielshir@gmail.com>
2012-11-22 18:57:21 +0200

    Updated Nextpeer SDK version

---
+++ Nextpeer.framework/Nextpeer.framework/Headers/NPCurrencyDelegate.h
@@ -0,0 +1,27 @@
+//
+//  IMPORTRANT NOTE:
+// 
+//  This delegate should be implemented by games that have UNIFIED virtual currency with Nextpeer.
+//  This means that the game is the one that stores the amount of currency a player and Nextpeer
+//  manipulates this number using this delegate.
+//
+
+@protocol NPCurrencyDelegate <NSObject>
+@required
+
+// Nextpeer will call this function to retrieve the current amount of currency the player has
+- (NSUInteger)nextpeerGetCurrency;
+
+// Nextpeer will use this function to add a certain amount of currency to the total a player has. 
+// This may be a result of currency consumption (negative amount, for example when entering a tournament) 
+// or a currency gain (positive amount, when winning a tournament).
+// 
+// NOTE:    This method isn't necessarily called on the main thread! Take precaution when receiving this callback
+//          and be sure to return quickly (i.e. do I/O in another thread) in order to have everything run smoothly.
+- (void)nextpeerAddAmountToCurrency:(NSInteger)amount;
+
+// Nextpeer will query this method to find out if the game supports the unified currecny model.
+// Note that this will only be called once per Nextpeer initialization.
+- (BOOL)nextpeerSupportsUnifiedCurrency;
+
+@end
